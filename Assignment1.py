eee147c123c52a57cf2383da6e20c8ca870ef34d
suyash-gholap <98027957+suyash-gholap@users.noreply.github.com>
2022-01-19 17:53:09 +0530

    Assignment 1 Submitted

---
+++ Assignment1.py
@@ -0,0 +1,53 @@
+# ----> int
+# a = input("enter the number:")
+
+# a = int(a)
+
+# square = a*a
+# print("the square of a is ", square)
+
+
+# ----> float
+# a = 45
+# b = 8
+# c = float(a/b)
+# print(c)
+
+
+# ----> boolean
+# def isdivisible(a,b):
+#     if a%b == 0:
+#         result = True
+#     else:
+#         result = False
+#     return result
+
+# print(isdivisible(20,5))
+
+
+
+# ----> list
+# a = [1,2,3,4,5,6]
+# a = [1,2,3,4,5]
+# print(a)
+
+# print(a[2])
+# a[0] = 353
+# print(a[0])
+# c = [54, "Suyash", False, 8.9]
+# print(c)
+
+
+# ----> tuple
+# t = (1,2,4,5)
+# print(t)
+
+
+# ----> dict
+# myDict = {
+#     "Steve": "Good Boy",
+#     "Marks": [1,2,4],
+# }
+# print(myDict['Steve'])
+# myDict['Marks'] = [45,80]
+# print(myDict['Marks'])
