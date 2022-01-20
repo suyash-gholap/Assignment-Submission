7f7ee2ba781dda08402f88fd19d4521e9be62f59
suyash-gholap <98027957+suyash-gholap@users.noreply.github.com>
2022-01-20 12:17:50 +0530

    Day 3- Assignment
    Name: Suyash Gholap

---
+++ Assignment2.py
@@ -0,0 +1,8 @@
+#take user input (eg. name, age, marks, family, etc)
+# Put this input in the list and print it using f-string
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+marks = input("Enter your marks: ")
+print(f'My name is {name} and my age is {age}, i got {marks} marks in class 12th')
+items = [name, age, marks]
+print(items)
