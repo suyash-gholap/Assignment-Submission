282d20764a673a24cee071f1f3c5fdfd7b82e979
suyash-gholap <98027957+suyash-gholap@users.noreply.github.com>
2022-01-22 20:00:42 +0530

    Day 4 - Assignment
    Suyash Gholap

---
+++ Assignment3.py
@@ -0,0 +1,26 @@
+# num = 21
+# name = "John"
+# num1 = 65.67
+# x = True
+# y = {"orange", "banana", "cherry"}
+# z = [1, 2, 3, 4]
+# w = ("good", "nice", "amazing")
+# print("Integer:", num)
+# print("String:", name)
+# print("float:", num1)
+# print("boolean:", x)
+# print("set:", y)
+# print("list:", z)
+# print("tuple:", w)
+
+
+#list
+# numbers = [1, 6, 9, 10, 17]
+# print(f'the numbers in the list are  {*numbers,}')
+
+
+#list
+# num1 = 5
+# num2 = 10
+# list1 = [num1, num2]
+# print(list1)
